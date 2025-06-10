596f4609a51b5cfb955443ea460392560d3f5921
Prakhar Srivastava <prakharbksc9@gmail.com>
2025-06-10 10:46:17 +0530

    update and delete

--- src/main/java/com/prakhar/ecom_Project/controller/ProductController.java
+++ src/main/java/com/prakhar/ecom_Project/controller/ProductController.java
@@ -3,8 +3,14 @@ package com.prakhar.ecom_Project.controller;
 import com.prakhar.ecom_Project.model.Product;
 import com.prakhar.ecom_Project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.List;
 
 @RestController
@@ -16,12 +22,67 @@ public class ProductController {
 
 
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(),HttpStatus.OK);
     }
+
     @GetMapping("/product/{id}")
-    public Product getProduct(@PathVariable int id){
-        return service.getProductById(id);
+    public ResponseEntity<Product> getProduct(@PathVariable int id){
+
+        Product product = service.getProductById(id);
+
+        if(product != null)
+            return new ResponseEntity<>(product,HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
+        try {
+            System.out.println(product);
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("product/{productId}/image")
+    public ResponseEntity<byte[]>getImageByProductId(@PathVariable int productId){
+
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageData();
+        return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType())).body(imageFile);
+
+    }
+    @RequestMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile) throws IOException {
+        Product product1 = null;
+        try {
+            product1 = service.updateProduct(id, product, imageFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>("Failed to update.....", HttpStatus.BAD_REQUEST);
+        }
+
+        if (product1!= null)
+            return new ResponseEntity<>("updated", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("failed to update", HttpStatus.BAD_REQUEST);
+    }
+
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product = service.getProductById(id);
+        if(product!= null) {
+            service.deleteProduct(id);
+            return new ResponseEntity<>("deleted", HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- src/main/java/com/prakhar/ecom_Project/model/Product.java
+++ src/main/java/com/prakhar/ecom_Project/model/Product.java
@@ -1,13 +1,8 @@
 package com.prakhar.ecom_Project.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -15,20 +10,52 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Getter
+@Setter
 public class Product {
 // 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private String desc;
+    private String description;
     private String brand;
     private BigDecimal price;
     private String category;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern ="dd-MM-yyyy")
     private Date releaseDate;
-    private boolean available;
-    private int quantity;
+    private boolean productAvailable;
+    private int stockQuantity;
 
+
+    private String imageName;
+    private String imageType;
+
+    @Lob
+    private byte[] imageData;
+
+//    public String getImageName() {
+//        return imageName;
+//    }
+//
+//    public void setImageName(String imageName) {
+//        this.imageName = imageName;
+//    }
+//
+//    public String getImageType() {
+//        return imageType;
+//    }
+//
+//    public void setImageType(String imageType) {
+//        this.imageType = imageType;
+//    }
+
+//    public byte[] getImageData() {
+//        return imageData;
+//    }
+//
+//    public void setImageData(byte[] imageData) {
+//        this.imageData = imageData;
+//    }
 }
